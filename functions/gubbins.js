11e6af6b891190111f204da328f0366b1b53ef4c
Caesura Media Net <andyc@caesuramedia.net>
2023-06-30 19:36:04 +0100

    Updated todo

--- functions/gubbins.js
+++ functions/gubbins.js
@@ -1,5 +1,5 @@
 /**
-* The thing that does the thing.
+* The thing that does the thing. Essentially Base64 plus an offset
 *
 * @format
 * @flow strict-local
@@ -7,25 +7,25 @@
 
 import { Base64 } from 'js-base64';
 
-const now         = new Date();
-const epochOffset = Math.floor(now/8.64e7) - 19000;
-const hex         = epochOffset.toString(16)
-const hexArr      = hex.split ('');
-const base64      = Base64.encode(hex);
+// Valid for 24hrs only.
+//
+function getDateOffset () {
+   var now         = new Date();
+   var epochOffset = Math.floor(now/8.64e7) - 19000;
+   var hex         = epochOffset.toString(16)
+   var base64      = Base64.encode(hex);
+   return {base64, epochOffset};
+}
 
 export function scrembl(message) {
+   let {base64, epochOffset} = getDateOffset();
    var chars       = message.split('');
    var output_text = "^";
-   var offset      = 0;
    for (var i=0; i < chars.length; i++) {
-      switch (i) {
-         case 0  : offset = parseInt (hexArr[0], 16); //eslint-no-fallthrough
-         case 1  : offset = parseInt (hexArr[1], 16); //eslint-no-fallthrough
-         case 2  : offset = parseInt (hexArr[2], 16); //eslint-no-fallthrough
-         case 3  : offset = parseInt (hexArr[3], 16); //eslint-no-fallthrough
-         default : offset = 0;
-      }
-      var asci = (parseInt (offset + message.charCodeAt(i)) + parseInt (epochOffset)).toString(16);
+
+      // Add a date offset to each char.
+      //
+      var asci = (parseInt (message.charCodeAt(i)) + parseInt (epochOffset)).toString(16);
       output_text += asci + "%";
    }
    output_text = output_text.replace (/%$/, '$') + base64;
@@ -33,22 +33,18 @@ export function scrembl(message) {
 }
 
 export function unScrembl(message) {
+   let {base64, epochOffset} = getDateOffset();
    var enc_base64  = message.replace(/^.*\$/, '');
    var output_text = "";
    if (enc_base64 !== base64) {
       output_text = "Out of date Scrembl, sorry!";
    } else {
       var asciis      = message.replace(/^\^/, '').replace(/\$.+$/, '').split('%');
-      var offset      = 0;
       for (var i=0; i < asciis.length; i++) {
-         switch (i) {
-            case 0  : offset = parseInt (hexArr[0], 16); //eslint-no-fallthrough
-            case 1  : offset = parseInt (hexArr[1], 16); //eslint-no-fallthrough
-            case 2  : offset = parseInt (hexArr[2], 16); //eslint-no-fallthrough
-            case 3  : offset = parseInt (hexArr[3], 16); //eslint-no-fallthrough
-            default : offset = 0;
-         }
-         var ch = String.fromCharCode((parseInt (asciis[i], 16) - offset - parseInt (epochOffset)));
+         var ch = String.fromCharCode((parseInt (asciis[i], 16) - parseInt (epochOffset)));
+
+         // Printable characters only.
+         //
          if (!ch.match(/^[\P{Cc}\P{Cn}\P{Cs}]+$/gu)) {
             output_text = "Out of date Scrembl, sorry!";
             break;
